5a46604fc97d58c7749adc1bbb25088d33b73249
Elouaeramine <49250202+Elouaeramine@users.noreply.github.com>
2020-05-25 17:57:01 +0100

    Updates

---
+++ precache-manifest.ad7be47776f2153a72cf333b51cbbbe8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c71aa6abdd6110677a931adb27912b6",
+    "url": "/RecipeApp/index.html"
+  },
+  {
+    "revision": "ccab3516daa141d9f8f6",
+    "url": "/RecipeApp/static/css/main.5c670903.chunk.css"
+  },
+  {
+    "revision": "a932410b8e27e6859cde",
+    "url": "/RecipeApp/static/js/2.26259b8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RecipeApp/static/js/2.26259b8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccab3516daa141d9f8f6",
+    "url": "/RecipeApp/static/js/main.63c5e2be.chunk.js"
+  },
+  {
+    "revision": "6ae4158d9168df0401c6",
+    "url": "/RecipeApp/static/js/runtime-main.08c75b51.js"
+  }
+]);
